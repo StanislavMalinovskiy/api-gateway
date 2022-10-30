aa37d83a8893d3975cdecbe982c8cc8ffba00009
Stanislav <stasma@hotmail.com>
2022-10-30 19:07:35 +0500

    jwt impl

--- src/main/java/uz/uzgps/apigateway/SecurityConfig.java
+++ src/main/java/uz/uzgps/apigateway/SecurityConfig.java
@@ -2,23 +2,60 @@ package uz.uzgps.apigateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("*"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setAllowedMethods(Arrays.asList("*"));
+//        configuration.setExposedHeaders(Arrays.asList("*"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/", configuration);
+//        source.registerCorsConfiguration("/*", configuration);
+//        source.registerCorsConfiguration("/**", configuration);
+//        source.registerCorsConfiguration("/***", configuration);
+//        source.registerCorsConfiguration("/****", configuration);
+//        source.registerCorsConfiguration("/*****", configuration);
+//
+//        return source;
+//    }
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-
-        http
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity
                 .authorizeExchange()
-                .anyExchange()
-                .authenticated()
                 .and()
-                .oauth2Login(); // to redirect to oauth2 login page.
-
-        return http.build();
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return httpSecurity.build();
     }
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//
+//        http
+//                .authorizeExchange()
+//                .anyExchange()
+//                .authenticated()
+//                .and()
+//                .oauth2Login(); // to redirect to oauth2 login page.
+//
+//        return http.build();
+//    }
 
 }
