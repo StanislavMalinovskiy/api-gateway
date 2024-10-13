38e7bcf8e49471a515922da196040f2f6f791be2
stanislavma <msvdev@yandex.com>
2024-10-13 21:16:40 +0500

    init commit

--- src/main/java/uz/uzgps/apigateway/SecurityConfig.java
+++ src/main/java/uz/uzgps/apigateway/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -32,6 +34,30 @@ public class SecurityConfig {
 //
 //        return source;
 //    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000", "http://192.168.0.108:8080"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        // Разрешить отправку cookies с запросами (если необходимо)
+        config.setAllowCredentials(true);
+
+        // Применить конфигурацию CORS ко всем путям
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
